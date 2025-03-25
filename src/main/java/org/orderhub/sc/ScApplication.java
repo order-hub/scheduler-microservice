e595bcad8b1f329498134226d6193aa13e328492
yyubin <hazing120@gmail.com>
2025-03-26 03:51:01 +0900

    config: initial project

---
+++ src/main/java/org/orderhub/sc/ScApplication.java
@@ -0,0 +1,13 @@
+package org.orderhub.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/orderhub/sc/ScApplicationTests.java
@@ -0,0 +1,13 @@
+package org.orderhub.sc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
